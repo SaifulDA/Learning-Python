63a36fb4f86bc75d17343aff9dc6ba01f5585db7
SaifulDA <saifuldaulah24@gmail.com>
2024-01-28 13:32:51 +0700

    add input and output

--- main.py
+++ main.py
@@ -2,4 +2,20 @@
 ubah  "Hello World!" menjadi pesan yang ingin anda sampaikan kepada dunia.
 """
 
-print("Hello seluruh dunia")
+#print("Hello seluruh dunia")
+
+greeting = "Hello seluruh dunia" #ini adalah assignment
+print(greeting) #ini adalah pemanggilan variabel
+
+"""
+addition adalah variabel yang bernilai ekspresi 2+2,
+result adalah hasil pengurangan dari variabel addition dikurangi 1
+"""
+addition = 2 + 2
+result = addition - 1
+print(result)
+
+# membuat input
+name = input("Masukkan nama anda: ")
+# membuat output
+print("Halo, " + name + "!")
