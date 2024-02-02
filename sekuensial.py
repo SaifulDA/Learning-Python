8a99b81ad3e9bf134e8ec3ba50f72c511e2975c4
SaifulDA <saifuldaulah24@gmail.com>
2024-02-02 10:47:14 +0700

    add learning sekuensial

---
+++ sekuensial.py
@@ -0,0 +1,15 @@
+# program input data diri
+print("selamat datang di program input data diri0\n")
+print ("silahkan masukkan data diri anda")
+nama = input("nama : ")
+umur = input("umur : ")
+alamat = input("alamat : ")
+
+print("data diri anda adalah")
+print("nama : ", nama)
+print("umur : ", umur)
+print("alamat : ", alamat)
+
+# block code 
+for i in range(10):
+    print(i)
