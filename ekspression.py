e600560b9d2f1f4e8cd8724c8a01f5ef2060b73b
SaifulDA <saifuldaulah24@gmail.com>
2024-02-02 10:25:19 +0700

    add learning ekspression

---
+++ ekspression.py
@@ -0,0 +1,114 @@
+# example 1
+x = 1
+y = 2
+result = x + y
+print(result)
+
+# example 2
+angka = [1, 2, 3, 4, 5]
+huruf = ['satu', 'dua', 'tiga', 'empat', 'lima']
+gabung = angka + huruf
+print(gabung)
+
+# example 3
+huruf = ['a', 'b', 'c', 'd', 'e']
+gabung = huruf * 2
+print(gabung)
+
+# ekspression uner
+a = True
+a = not a
+print(a)
+
+b = 6
+b -= 1
+print(b)
+
+c = 6
+c += 1
+print(c)
+
+d = 6
+print(-d)
+
+# ekspression 
+print(2 + 3 * 4)
+print(1<10)
+print(True or False)
+
+# operator aritmatika
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# operator relasional
+x = 10
+y = 3
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = "Dicoding"
+y = "Indonesia"
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+# operator logika
+# operator and
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# operator or
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# operator not
+print(not True)
+print(not False)
+
+# operator assignment
+x = 10
+x += 5
+print(x)
+
+x = 10
+x -= 5
+print(x)
+
+x = 10
+x *= 5
+print(x)
+
+x = 10
+x /= 5
+print(x)
+
+x = 10
+x %= 5
+print(x)
+
+x = 10
+x //= 5
+print(x)
+
+x = 10
+x **= 5
+print(x)
