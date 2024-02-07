d8a74fe809cfa35787723d6662a2acd8ed2a0921
SaifulDA <saifuldaulah24@gmail.com>
2024-02-07 11:24:54 +0700

    add leaning modul

---
+++ learning_modul/main.py
@@ -0,0 +1,12 @@
+import hello
+from hello import mencari_luas_persegi_panjang
+
+persegi_panjang_pertama = hello.mencari_luas_persegi_panjang(5,10)
+print(persegi_panjang_pertama)
+
+print(hello.nama)
+
+persegi_panjang_pertama = mencari_luas_persegi_panjang(5,10)
+print(persegi_panjang_pertama)
+ 
+print(nama)
